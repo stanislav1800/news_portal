813112acdc988a0d6f0813629191a6a9fbb8223f
Станислав <77958472+stanislav1800@users.noreply.github.com>
2024-02-24 18:28:25 +0700

    Delete simpleapp directory

--- simpleapp/cheduler.py
+++
@@ -1,7 +0,0 @@
-from apscheduler.schedulers.background import BackgroundScheduler
-
-appointment_scheduler = BackgroundScheduler()
-appointment_scheduler.add_job(
-    id='send mails', func=lambda :print(), trigger='interval', seconds=5
-)
-
